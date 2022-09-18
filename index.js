87473d2475d8c13045f00d0f853eee38ca67687f
Samuel Munyoki Muthembwa <86914033+samuelmunyoki@users.noreply.github.com>
2022-09-18 23:02:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const config = require("config");
 const connectDB = require("./database/dbconnect");
 const apiRoute = require("./routes/api")
 const redirectRoute = require("./routes/redirect");
+const PORT = process.env.PORT || 80
 const app = express();
 app.use(express.json());
 connectDB();
 app.use("/api/v1", apiRoute);
 app.use("/", redirectRoute);
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("++++++++ Server Started on port 3000 ++++++++");
 });
 
