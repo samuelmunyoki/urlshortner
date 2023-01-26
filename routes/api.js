b2fa8a6183f3e89bb660ff5faa377fe73c87e137
Samuel Munyoki Muthembwa <86914033+samuelmunyoki@users.noreply.github.com>
2023-01-26 15:03:42 +0300

    Minor Fixes

--- routes/api.js
+++ routes/api.js
@@ -35,7 +35,7 @@ router.post("/", async (req, res) => {
           .status(200)
           .send({ message: "🐥 Short link: ", sharelink: { shortUrl } });
       } else {
-        res.status(200).send({ message: "❗ Invalid URL ❗" });
+        res.status(200).send({ message: "❗ Invalid URL ❗",sharelink: { "" } });
       }
     } catch (error) {
       res.status(200).send({ message: "🦀 Could not generate Shortened URL 🦀" });
